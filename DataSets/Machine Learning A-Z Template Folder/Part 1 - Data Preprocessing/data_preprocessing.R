0fd8f6ec074c44fda2ca183ff6469eaf167c364e
Heera Hemanth Bylla <heerahemanth1@gmail.com>
2017-06-20 06:32:32 +0530

    Simple Linear Regression completed

--- DataSets/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing.R
+++ DataSets/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing.R
@@ -5,9 +5,9 @@ dataset = read.csv('Data.csv')
 
 # Taking care of missing data
 dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, 
-                                             FUN = function(x) mean(x, na.rm = TRUE)), dataset$Age)
+                                             fu = function(x) mean(x, na.rm = TRUE)), dataset$Age)
 dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, 
-                                             FUN = function(x) mean(x, na.rm = TRUE)), dataset$Salary)
+                                             fu = function(x) mean(x, na.rm = TRUE)), dataset$Salary)
 
 # Encoding categorical data
 dataset$Country = factor(dataset$Country,
--- DataSets/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
+++ DataSets/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 10)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,3 +21,27 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting simple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the Test Set
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training Set result
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing the Test Set result
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
